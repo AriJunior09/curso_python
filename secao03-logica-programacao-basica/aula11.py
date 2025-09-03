38f16485cc1abdbcdba7ba23ec45f99600cdec85
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-03 20:24:17 -0300

    Update aula11.py

--- secao03-logica-programacao-basica/aula11.py
+++ secao03-logica-programacao-basica/aula11.py
@@ -2,4 +2,15 @@
 # Entrada de dados pelo usuário
 
 
-nome = input('Qual é o seu nome? ')
+# nome = input('Qual é o seu nome? ') # A função input() sempre retorna uma string
+# print(f'Olá, {nome}')
+
+
+x = input('Digite um número: ')
+y = int(input('Digite outro número: '))
+
+int_x = int(x) # Convertendo string para inteiro
+int_y = int(y) # Convertendo string para inteiro
+
+print(f'A soma é {int_x + int_y}') # Mostrando a soma dos números
+
