39356f0a2e8371c04c7c4a802048edefb5440824
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-03 22:09:27 -0300

    up

--- aula14.py
+++ aula14.py
@@ -1,1 +1,14 @@
-# Usuario digita dois valores e o sistema diz qual valor é maior e qual é menor
+# Usuario digita dois valores e o sistema diz qual valor é maior
+
+
+primeiro = input("Digite o primeiro valor: ")
+segundo = input("Digite o segundo valor: ")
+
+if  primeiro > segundo:
+    print('O primeiro é maior')
+
+elif segundo > primeiro:
+    print('O segundo valor é maior')
+
+else:
+    print('Os números são iguais')
