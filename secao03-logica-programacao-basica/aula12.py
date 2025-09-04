8a2ae27ebe82878bcdc9005cf558cdebc66936e3
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-03 21:20:55 -0300

    update

--- secao03-logica-programacao-basica/aula12.py
+++ secao03-logica-programacao-basica/aula12.py
@@ -1,11 +1,49 @@
 # Aprendendo condições
+# if, elif, else
+# Condições simples e compostas
 
-entrada = input('Você quer "entrar" ou "sair"?: ').lower()
+idade = 18
+maior_idade = 18
+
+# Condição simples
+if idade >= 18: print('É maior de idade')
 
 
-if entrada == 'entrar':
-    print('Você entrou no sistema')
-elif entrada == 'sair':
-    print('Você saiu do sistema')
+# Condição simples com bloco
+if idade >= maior_idade:
+    print('É maior de idade')
 else:
-    print('Digite uma opção válida!')
+    print('É menor de idade')
+
+
+# Condição composta
+if idade < 18:
+    print('É menor de idade')
+elif idade == 18:
+    print('Acabou de se tornar maior de idade')
+else:
+    print('É maior de idade')
+
+
+# Operador Ternário
+print('É maior de idade') if idade >= 18 else print('É menor de idade')
+
+
+# Condição aninhada
+if idade < 18:
+    print('É menor de idade')
+    if idade == 17:
+        print('Está quase se tornando maior de idade')
+    else:
+        print('Ainda falta um tempo para se tornar maior de idade')
+else:
+    print('É maior de idade')
+    if idade == 18:
+        print('Acabou de se tornar maior de idade')
+    else:
+        print('Já é maior de idade há algum tempo') 
+        if idade >= 65:
+            print('É idoso')
+        else:
+            print('Ainda não é idoso')
+# Fim
