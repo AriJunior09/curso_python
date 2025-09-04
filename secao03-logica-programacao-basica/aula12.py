3413e3b603e450ce5e1f6471ea3136b83927364a
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-03 21:25:53 -0300

    update

--- secao03-logica-programacao-basica/aula12.py
+++ secao03-logica-programacao-basica/aula12.py
@@ -2,35 +2,35 @@
 # if, elif, else
 # Condições simples e compostas
 
-idade = 18
+idade = 33
 maior_idade = 18
 
 # Condição simples
-if idade >= 18: print('É maior de idade')
+if idade >= maior_idade: print('É maior de idade')
 
 
-# Condição simples com bloco
+# # Condição simples com bloco
 if idade >= maior_idade:
     print('É maior de idade')
 else:
     print('É menor de idade')
 
 
-# Condição composta
-if idade < 18:
+# # Condição composta
+if idade < maior_idade:
     print('É menor de idade')
-elif idade == 18:
+elif idade == maior_idade:
     print('Acabou de se tornar maior de idade')
 else:
     print('É maior de idade')
 
 
-# Operador Ternário
-print('É maior de idade') if idade >= 18 else print('É menor de idade')
+# # Operador Ternário
+print('É maior de idade') if idade >= maior_idade else print('É menor de idade')
 
 
-# Condição aninhada
-if idade < 18:
+# # Condição aninhada
+if idade < maior_idade:
     print('É menor de idade')
     if idade == 17:
         print('Está quase se tornando maior de idade')
@@ -38,7 +38,7 @@ if idade < 18:
         print('Ainda falta um tempo para se tornar maior de idade')
 else:
     print('É maior de idade')
-    if idade == 18:
+    if idade == maior_idade:
         print('Acabou de se tornar maior de idade')
     else:
         print('Já é maior de idade há algum tempo') 
