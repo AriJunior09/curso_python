4ce31c9215d01bc3cf01fe20003dede9bc2704f5
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-05 22:15:28 -0300

    update

--- secao_03/aula18.py
+++ secao_03/aula18.py
@@ -3,3 +3,25 @@
 
 # Strings são interaveis
 
+#..0.1.2.3.4.5
+#..O.t.á.v.i.o
+#.-6-5-4-3-2-1
+
+nome = 'Otávio'
+# print(nome[2])
+# print(nome[-4])
+
+# print('á' in nome)
+# print('z' in nome)
+# print(20 * '-')
+# print('á' not in nome)
+# print('z' not in nome)
+
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que você quer encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
