fc4ce419747cba08215b22db412b79b98d535e10
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-05 22:53:40 -0300

    up

---
+++ secao_03/aula21.py
@@ -0,0 +1,20 @@
+# Fatiamento de String
+"""
+012345678
+Olá mundo
+-987654321
+Fatiamento [inicio:fim:passo] [::]
+Obs.: a função len retorna a qtd de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+# print(variavel[5])
+# print(variavel[-4])
+
+# print(variavel[4:])
+# print(variavel[2:5])
+
+# print(len(variavel[2:5]))
+print(variavel[::-1])
+
+
