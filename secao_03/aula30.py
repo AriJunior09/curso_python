247c0199a59784c5c3abcb768f38ed4bad73360c
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-12 21:41:43 -0300

    create aula31

--- secao_03/aula30.py
+++ secao_03/aula30.py
@@ -4,6 +4,15 @@ Imutáveis que vimos: str, int, float, bool
 """
 
 string = 'Ari'
+#string[1] = 'G'  # Não pode alterar pois esse tipo é imutável
+
+nome = 'Junior'
+# nome_2 = f'{nome[:3]}ino é P{nome[3:]}'
+
+
+
+
+
 int_idade = 33
 float_peso = 84.562
 boolean_casado = True
---
+++ secao_03/aula31.py
@@ -0,0 +1,5 @@
+"""
+Repetições:
+While (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+"""
