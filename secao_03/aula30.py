0e36c0f3ba86a9fd5894984c7f91f7c47706430f
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-12 21:20:47 -0300

    update aula30

--- secao_03/aula30.py
+++ secao_03/aula30.py
@@ -1,3 +1,9 @@
 """
-build
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
 """
+
+string = 'Ari'
+int_idade = 33
+float_peso = 84.562
+boolean_casado = True
