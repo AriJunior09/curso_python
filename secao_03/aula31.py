10d5ebcaf5bae9c3aefd17c6b1017460ff575fdf
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-12 21:48:26 -0300

    update

--- secao_03/aula31.py
+++ secao_03/aula31.py
@@ -2,4 +2,14 @@
 Repetições:
 While (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+Para parar o loop, digite no terminal: Ctrl + C -> KeyboardInterrupt
 """
+
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+print('Fim')
