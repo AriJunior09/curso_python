42d73feec28e75674195fb62f0d6b41c39f6816c
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-12 22:22:46 -0300

    up

---
+++ secao_03/aula33.py
@@ -0,0 +1,24 @@
+"""
+Operadores de atribuição
+= += -= /= //= **= %=
+"""
+
+a = b = c = d = e = f = g = 10
+
+a += 1
+b -= 1
+c *= 2
+d /= 2
+e //= 3
+f **= 2
+g %= 2
+
+print(f'10 + 1 = {a}')
+print(f'10 - 1 = {b}')
+print(f'10 * 2 = {c}')
+print(f'10 / 2 = {d}')
+print(f'10 // 3 = {e}')
+print(f'10 ** 2 = {f}')
+print(f'10 % 2 = {g}')
+
+
