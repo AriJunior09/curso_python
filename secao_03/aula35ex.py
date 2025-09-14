42aca1e467c2224d5a304fcfa6b5feabf687414d
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-14 15:28:32 -0300

    create aula35

---
+++ secao_03/aula35ex.py
@@ -0,0 +1,19 @@
+""""
+Interando strings com while
+"""
+#.......0123456789 (indices positivos)
+nome = 'Ari Junior'  # Strings são iteraveis
+#.....-10987654321 (indices negativos)
+
+indice = 0
+nova_string = ''
+
+while indice < len(nome):
+    letra = nome[indice]
+    nova_string += f'*{letra}'
+    indice += 1
+
+nova_string += '*'
+print(nova_string)
+
+
