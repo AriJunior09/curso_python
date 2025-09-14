3b0ba664001935a55e2edf2980301b75b6c395f6
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-14 14:38:28 -0300

    create aula34

---
+++ secao_03/aula34.py
@@ -0,0 +1,15 @@
+# While dentro de While
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+
+while linha <= qtd_linhas:
+    coluna = 1
+    print('-' * 17)
+    while coluna <= qtd_colunas:
+        print(f'{linha=}, {coluna=}')
+        coluna += 1
+    linha += 1
+print('Acabou')
