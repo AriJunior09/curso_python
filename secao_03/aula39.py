a8b517a93ec0e8b54f352fe7d0b7a3de76fd2cbd
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-14 19:39:31 -0300

    create aula39

---
+++ secao_03/aula39.py
@@ -0,0 +1,17 @@
+""" for / in """
+
+'''Para interar um texto com while seria'''
+# texto = 'Python'
+
+# i = 0
+# while i < len(texto):
+#     print(texto[i], i)
+
+#     i += 1
+# -----------------------------------------------
+
+''' Agora vamos ver com for'''
+texto = 'Python'
+
+for letra in texto:
+    print(letra, texto.index(letra))
