426bdbf1b86264972e3742bd4e7b0ba41fd99de3
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-18 22:42:50 -0300

    create aula44

---
+++ lista/aula44.py
@@ -0,0 +1,13 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, 
+"""
+#........0.....1......2.....3....4
+lista = [123, True, 'Ari', 1.2, [80]] # lista com vários valores
+
+print(lista)
+print(lista[2].upper(), type(lista[2]))
+
