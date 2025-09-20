c115b030eb4f836b0771dcc6eaf81afc147099a1
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-19 21:43:28 -0300

    create aula44

--- lista/aula44.py
+++ lista/aula44.py
@@ -4,10 +4,24 @@ Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - indices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, 
+Create Read Update Delete (CRUD)
+Criar  Ler  Alterar Deletar
 """
 #........0.....1......2.....3....4
-lista = [123, True, 'Ari', 1.2, [80]] # lista com vários valores
+# lista = [123, True, 'Ari', 1.2, [80]] # lista com vários valores
+# print(lista)
+# print(lista[2].upper(), type(lista[2]))
 
+#.........0...1...2...3
+
+
+lista = [12, 23, 48, 55]
+
+lista.append(6)
+lista.pop()
+lista.append(60)
 print(lista)
-print(lista[2].upper(), type(lista[2]))
+
+
+
 
