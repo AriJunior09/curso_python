7ac63810bf10ea29ca5e96ef43439d793deb5396
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-23 21:30:16 -0300

    create aula49

---
+++ secao_03/aula49.py
@@ -0,0 +1,10 @@
+""" Introdução ao desempacotamento """
+
+# nomes = ['Ari', 'Rute', 'Ezequiel']
+# nome1, nome2, nome3 = nomes
+
+# print(nome1.upper())
+
+n1, n2, *_ = ['a', 'b', 'c', 'd', 'e'] # o _ é o *resto (uma variavel que não será usada)
+
+print(_)
