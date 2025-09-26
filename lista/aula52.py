ca252ba26663e9ac8f5fe703432452a9d8dbcfd3
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-25 23:21:12 -0300

    update

--- lista/aula52.py
+++ lista/aula52.py
@@ -4,6 +4,7 @@ O usuário deve ter a possibilidade de inserir,
 apagar e listar valores da sua lista
 Não permita que o programa quebre com erros de indices inexistentes na lista.
 """
+
 import os
 lista = []
 
