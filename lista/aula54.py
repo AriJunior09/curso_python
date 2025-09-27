dce21c475f70a6d13f2856a14311f698fbda449b
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-26 21:20:15 -0300

    create aula54

---
+++ lista/aula54.py
@@ -0,0 +1,21 @@
+"""
+split e join com list e str
+split() - divide uma string em lista de palavras
+join(iteravel) - une uma string
+strip() - remove espaços no começo e no fim
+rstrip() - remove espaços na direita
+lstrip() - remove espaços na esquerda
+"""
+
+frase = '   Olha só  ,  que coisa interessante!    '
+lista_frases_cruas = frase.split(', ')
+
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+# print(lista_frases_cruas)
+# print(lista_frases)
+
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
