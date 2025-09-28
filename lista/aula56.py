f5f5b0f3c11da6d2af385624ccde71ef6daad602
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-09-28 20:16:34 -0300

    up aula56

--- lista/aula56.py
+++ lista/aula56.py
@@ -2,8 +2,21 @@
 Desempacotando em chamadas de métodos e funções"""
 
 string = 'ABCD'
-lista = ['Maria', 'Helena', 'Eduarda']
+lista = ['Maria', 'Helena', 1, 2, 3, 4, 'Eduarda']
 tupla = ('Python', 'é', 'legal')
+salas = [
+    #  0       1
+    ['Ari', 'Rute'],            # 0
+    #  0
+    ['Yasmim'],                 # 1
+    #  0        1         2     
+    ['Enzo', 'Eduarda', 'Caio'] # 2
+]
 
-a, b, c = lista
-print(a)
+# prim, seg, *_, antepenutimo, ultimo = lista
+# print(prim, _)
+
+# for nome in lista:
+#     print(nome, end= ' ')
+
+print(*salas, sep='\n') 
