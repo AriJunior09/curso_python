f302113e521e4196878ba397d293049ecc1849d2
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-01 21:31:52 -0300

    update cpf_aula02

--- validando_CPF/cpf_aula02.py
+++ validando_CPF/cpf_aula02.py
@@ -25,13 +25,31 @@ Nesse caso o SEGUNDO dígito do CPF é 0
 
 """
 cpf = '74682489070'
-dez_digitos = cpf[:10]
+nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
+resultado_dig_1 = 0
+
+for digito in nove_digitos:
+    resultado_dig_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_dig_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+
+dez_digitos = nove_digitos + str(digito_1)
 contador_regressivo_2 = 11
 resultado_dig_2 = 0
-
-for digito_2 in dez_digitos:
-    resultado_dig_2 += int(digito_2) * contador_regressivo_2
+for digito in dez_digitos:
+    resultado_dig_2 += int(digito) * contador_regressivo_2
     contador_regressivo_2 -= 1
 digito_2 = (resultado_dig_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
-print(digito_2)
+
+
+novo_cpf = f'{nove_digitos}{digito_1}{digito_2}'
+print(novo_cpf)
+if novo_cpf == cpf:
+    print('CPF VÁLIDO')
+else:
+    print('CPF INVÁLIDO')
+# 74682489070
