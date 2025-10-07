300c531fa71fc08c7fd813690c570c006eb2edfb
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-06 21:08:33 -0300

    create aula07

--- secao_04/funcoes/aula06.py
+++ secao_04/funcoes/aula06.py
@@ -6,13 +6,16 @@ x = 1  # Variável global
 
 def escopo():
     x = 10  # Variável local
+    print(f'x de dentro do escopo = {x}')  # Acessa a variável local x=10
 
     def outra_funcao():
-        y = x * 2  # Acessa a variável local x=10
-        print(f'Outra função: x={x}, y={y}')  # Acessa x=10 e y=20
-    
+        global x  # Declara que queremos usar a variável global x
+        x = 100   # Modifica a variável global x
+        y = x * 2
+        print(f'y de dentro da outra_funcao = {y}')  # Acessa a variável local x=10
 
-    outra_funcao()  # Chama a função interna
-    print(f'Escopo: x={x}')  # Acessa a variável local x=10
+    outra_funcao()  
 
-
+print(f'x na variável global antes de modificar = {x}')  # Acessa a variável global x=1
+escopo()  # Chama a função externa x=10
+print(f'x na variável global depois = {x}')  # Acessa a variável global x=1
