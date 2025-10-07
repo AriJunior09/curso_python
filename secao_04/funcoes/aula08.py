38a5cdc8304639e892c4080d0727678bffcd1067
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-06 21:51:05 -0300

    create aula08

---
+++ secao_04/funcoes/aula08.py
@@ -0,0 +1,14 @@
+""" Retorno das funções:
+- Sempre que uma função não retorna um valor, o Python retorna None.
+- Funções que retornam valores, retornam o valor especificado.
+- Funções podem retornar múltiplos valores, na forma de tuplas.
+ """
+
+
+def soma(a, b):
+    return a + b
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+
+print(soma1 + soma2)
