cfb70cddab361318093ecfa9faa825343da96b1c
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-08 21:07:35 -0300

    create aula16

---
+++ secao_04/funcoes/aula16.py
@@ -0,0 +1,19 @@
+"""
+Crie funções que duplicam, triplicam e quadruplicam
+o número recebido como parâmetro
+"""
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+numero = int(input('Digite um número: '))
+
+print(f'Duplo       = {duplicar(numero)}')      
+print(f'Triplo      = {triplicar(numero)}')     
+print(f'Quadruplo   = {quadruplicar(numero)}')   
