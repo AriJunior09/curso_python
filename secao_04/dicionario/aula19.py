22e54cd590cd87c315ef77dfd1a828218679376e
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-08 22:45:00 -0300

    create aula19

---
+++ secao_04/dicionario/aula19.py
@@ -0,0 +1,15 @@
+"""
+Métodos úteis dos dicionários em Python
+len - Retorna o número de itens (pares chave-valor) no dicionário.
+keys - Retorna uma visão (view) das chaves no dicionário.
+values - Retorna uma visão (view) dos valores no dicionário.
+items - Retorna uma visão (view) dos pares chave-valor no dicionário.
+setdefault - Retorna o valor de uma chave se ela existir; caso contrário, insere a chave com um valor padrão.
+copy - Retorna uma cópia rasa (shallow copy) do dicionário.
+get - Retorna o valor de uma chave se ela existir; caso contrário, retorna um valor padrão (None se não for especificado).
+pop - Remove a chave especificada e retorna o valor associado a ela.
+popitem - Remove e retorna um par chave-valor arbitrário do dicionário.
+clear - Remove todos os itens do dicionário.
+update - Atualiza o dicionário com os pares chave-valor de outro dicionário ou de um iterável de pares chave-valor.
+"""
+
