0e879e9ba2622cda7b34207c5bfb3635baddf6de
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-11 19:31:42 -0300

    update aula19

--- secao_04/dicionario/aula19.py
+++ secao_04/dicionario/aula19.py
@@ -19,7 +19,29 @@ pessoa = {
     'idade': 33
 }
 
+# print(len(pessoa))          # Retorna o número de itens (pares chave-valor) no dicionário.
+# print(pessoa.keys())        # Retorna uma visão (view) das chaves no dicionário.
+# print(list(pessoa.keys()))  # Convertendo a visão das chaves para uma lista
+# print(pessoa.values())      # Retorna uma visão (view) dos valores no dicionário.
+# print(list(pessoa.values()))  # Convertendo a visão dos valores para uma lista
+# print(pessoa.items())         # Retorna uma visão (view) dos pares chave-valor no dicionário.
+# print(list(pessoa.items()))   # Convertendo a visão dos pares chave-valor para uma lista
 
-print(len(pessoa))  # Retorna o número de itens (pares chave-valor) no dicionário.
 
-print(pessoa.keys())    # Retorna uma visão (view) das chaves no dicionário.
+# for chave in pessoa.keys():
+#     print(chave)
+
+# for valor in pessoa.values():
+#     print(valor)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+print(pessoa['sobrenome'])        # Junior
+print(pessoa.get('sobrenome'))    # Junior
+print(pessoa.get('idade'))        # 33
+print(pessoa.get('altura'))       # None
+print(pessoa.get('altura', 1.75)) # 1.75 - se não existir a chave, retorna o valor padrão
+pessoa.setdefault('Signo', "Leão") # Se a chave não existir, cria a chave com o valor padrão
+
+print(pessoa)
