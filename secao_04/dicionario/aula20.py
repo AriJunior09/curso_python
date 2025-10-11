cbc3fa9ad5f623f418087d99db3a2c2c6776478f
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-10-11 20:16:43 -0300

    create aula21

--- secao_04/dicionario/aula20.py
+++ secao_04/dicionario/aula20.py
@@ -20,15 +20,15 @@ dic_03 = dic_01.copy() # Cópia rasa (shallow copy) - Cria um novo objeto na mem
 dic_04 = copy.deepcopy(dic_01)
 
 dic_01['nome'] = 'Ariel' # Alterando o valor de uma chave existente
-dic_01['lista'][3] = 999 #
+dic_01['lista'][3] = 999 # Alterando um valor dentro da lista (mutável) que é um valor do dicionário
 
 print('Dic_01', dic_01) # dic_01 reflete a mudança
 print('Dic_02', dic_02) # dic_02 é uma referência ao mesmo objeto que dic_01, então reflete as mudanças
 print('Dic_03', dic_03) # dic_03 é uma cópia independente, então não reflete as mudanças feitas em dic_01
-print('Dic_04', dic_04)
+print('Dic_04', dic_04) # dic_04 é uma cópia profunda, então não reflete as mudanças feitas em dic_01
 
 # Verificando os IDs (endereços na memória) dos dicionários
 print(id(dic_01)) # ID de dic_01
 print(id(dic_02)) # ID de dic_02 (mesmo que dic_01)
 print(id(dic_03)) # ID de dic_03 (diferente de dic_01 e dic_02)
-print(id(dic_04))
+print(id(dic_04)) # ID de dic_04 (diferente de todos os outros)
---
+++ secao_04/dicionario/aula21.py
@@ -0,0 +1,44 @@
+"""
+get - obtém uma chave
+pop - Apaga um item com a chave especificada (del)
+"""
+
+pessoa = {
+    'nome': 'Ari',
+    'sobrenome': 'Junior',
+    'idade': 33
+}
+# print(pessoa['cpf'])     # KeyError
+# print(pessoa.get('cpf')) # Quando a chave não existe, retorna None
+# print(pessoa.get('cpf', 'Não encontrado')) # Não encontrado
+
+'''-------------------------------------------------------------
+pop - Remove o item com a chave especificada e retorna o valor correspondente.'''
+# remover = pessoa.pop('sobrenome') # Remove a chave e retorna o valor
+# print(remover) # Junior
+# print(pessoa)  # {'nome': 'Ari', 'idade': 33}
+
+'''-------------------------------------------------------------
+popitem - Remove e retorna o último item do dicionário.'''
+
+# ultima_chave = pessoa.popitem()
+# print(ultima_chave) # ('idade', 33)
+# print(pessoa)       # {'nome': 'Ari', 'sobrenome': 'Junior'}
+
+'''-------------------------------------------------------------
+update - Atualiza o dicionário com os pares chave-valor de outro dicionário ou de um iterável de pares chave-valor.'''
+
+# pessoa.update({'nome': 'Caio', 'idade': 20, 'altura': 1.70}) # Atualiza os valores das chaves existentes e adiciona novas chaves
+
+# pessoa.update(nome='Alex', idade=34) # Forma alternativa
+
+# print(pessoa) # {'nome': 'Caio', 'sobrenome': 'Junior', 'idade': 20, 'altura': 1.70}
+
+# for chave, valor in pessoa.items():
+#     print(f'{chave}: {valor}')
+
+'''-------------------------------------------------------------'''
+
+lista = [ ['nome', 'Rafael'], ['sobrenome', 'Silva'], ['idade', 55] ]
+pessoa.update(lista) # Atualiza o dicionário com uma lista de listas
+print(pessoa) # {'nome': 'Rafael', 'sobrenome': 'Silva', 'idade': 55}
